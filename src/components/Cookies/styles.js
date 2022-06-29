a4620dfa41f0569f4f5c58e317786f77301aec7e
Antony Rafael <depaulacostaantony@gmail.com>
2022-06-29 08:40:42 -0300

    created privacy policy page
    Added a demo privacy policy page
    Added a link to privacy policy page on footer
    Added a link to privacy policy page on the cookie modal

---
+++ src/components/Cookies/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+import "react-rangeslider/lib/index.css";
+
+export const CookieStyles = styled.div`
+  h3 {
+    color: #fff;
+    font-weight: 500;
+    display: inline;
+  }
+
+  a {
+    display: inline;
+    color: white;
+    transition: all 0.3s;
+    text-decoration: underline;
+
+    &:hover {
+      color: #abc2cf;
+      text-decoration: none;
+    }
+  }
+`;
